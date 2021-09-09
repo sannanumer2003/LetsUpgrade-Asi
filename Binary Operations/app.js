2220642fa22e6f170c85e5a7e598954897afe84d
Muhammad Sannan Umer <75867692+sannanumer2003@users.noreply.github.com>
2021-09-10 01:14:32 +0500

    Add files via upload

---
+++ Binary Operations/app.js
@@ -0,0 +1,65 @@
+let a = 12;
+let b = 13;
+
+//1)Arithmetic Operations
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a%b);
+
+
+//2)Assignment Operations
+// a=b;
+// console.log(a);
+// a += b;
+// console.log(a);
+// a -= b;
+// console.log(a);
+// a *= b;
+// console.log(a);
+// a /= b;
+// console.log(a);
+// a %= b;
+// console.log(a);
+
+
+//3)Relational Operators
+// if(a >= b){
+//     console.log("a is greater than or equal to b")
+// }else if(a > b){
+//     console.log("a is greater than b");
+// }else if(a <= b){
+//     console.log("a is lesser than or equal to b");
+// }else if(a < b){
+//     console.log("a is lesser than b");
+// }else if(a == b){
+//     console.log("a is equal to b");
+// }else if( a!= b){
+//     console.log("a is not equal to b");
+// }
+
+
+//4)Identity Operators
+// a = 12;
+// b = '12';
+// if(a === b){
+//     console.log("a is equal and the same data type as of b");
+// }else if(a !== b){
+//     console.log("a maybe equal to b in value but not in data type");
+// }
+
+//5)Logical Operators
+let c = 14;
+let d = 14;
+// if(a > b && a > c){
+//     console.log("a is greater than b & c");
+// }else if(b > a && b > c){
+//     console.log("b is greater than a & c");
+// }else if(c > a && c > b){
+//     console.log("c is greater than a  & b");
+// }
+console.log(a > b || c == d); //True
+d = 0;
+console.log(!d);//True
+console.log(!a)//False
